65ff715f3188679331f79f7f4afb5153a24a9266
apicahaya <106047397+Agni-AIT@users.noreply.github.com>
2024-07-24 07:23:26 +0700

    Initial Commit

---
+++ music/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  music
+//
+//  Created by Agni Muhammad on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
